2772b0f5e3659ed60152f2ab74d36ffa5a389f65
hhoke <harris.hoke@10gen.com>
2021-12-15 15:36:49 -0500

    check API creds

---
+++ check_api_creds.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import boto3
+import botocore
+import csv
+import pprint
+import yaml
+
+from evergreen.api import EvgAuth, EvergreenApi
+
+with open("creds_bot.yml", "r") as f:
+    yml = yaml.safe_load(f)
+
+user = yml["user"]
+print(user)
+api_key = yml["api_key"]
+
+api = EvergreenApi.get_api(EvgAuth(user, api_key))
+
+taskname= "mongodb_mongo_master_enterprise_rhel_80_64_bit_dynamic_required_build_variant_gen_patch_c07540fefab04df035434955230f95a1d6297ed6_61ba26d83e8e864892e79cd8_21_12_15_17_33_31"
+print(api.task_by_id(taskname))
